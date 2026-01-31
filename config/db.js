cba305cd143f3e810d56d0d1a142478b55de9e2d
IvanTest <testivan2026@gmail.com>
2026-02-01 01:31:43 +0200

    fix: db config and comments controller

--- config/db.js
+++ config/db.js
@@ -1,26 +1,25 @@
-import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
+import { Sequelize } from 'sequelize'
+import dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config()
 
 export const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST, // ← mariadb
-    port: process.env.DB_PORT || 3306,
-    dialect: "mariadb",
-    logging: false,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mariadb',
   }
-);
+)
 
 export const testConnection = async () => {
   try {
-    await sequelize.authenticate();
-    console.log("✅ DB connection OK");
+    await sequelize.authenticate()
+    console.log('✅ DB connection OK')
   } catch (error) {
-    console.error("❌ DB connection failed:", error);
-    throw error; 
+    console.error('❌ DB connection failed:', error)
+    throw error
   }
-};
+}
